8212f00ade5902f9a4fa006a2ae83e22a6e0a119
hswick <hswick@smu.edu>
2017-09-27 11:16:01 -0500

    Replaced ExampleRequester with ExampleTaskGiver

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 var Truebit = artifacts.require("./Truebit.sol");
-var ExampleRequester = artifacts.require("./ExampleRequester.sol");
+var ExampleTaskGiver = artifacts.require("./ExampleTaskGiver.sol");
 var TaskGiver = artifacts.require("./TaskGiver.sol")
 var Solver = artifacts.require("./Solver.sol")
 var Verifier = artifacts.require("./Verifier.sol")
 
 module.exports = function(deployer, network, thing) {
   deployer.deploy(Truebit).then(function() {
-    return deployer.deploy(ExampleRequester, Truebit.address);
+    return deployer.deploy(ExampleTaskGiver, Truebit.address);
   });
 
   deployer.deploy(TaskGiver);
