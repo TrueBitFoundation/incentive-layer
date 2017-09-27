fac18900037986b45890c18492d82be856af8f2b
hswick <hswick@smu.edu>
2017-09-27 13:23:19 -0500

    Got TaskGiver instance working in tests

--- contracts/TaskGiver.sol
+++ contracts/TaskGiver.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.4.4;
 
 contract TaskGiver {
+
+  /*
+  	Task Data Spec
+  	--------------------
+    0 bytes32 dataRoot
+    1 address taskGiver
+    2 uint    minDeposit
+    3 uint    reward
+    4 uint timeout
+  */
+
   function proccessTask(uint taskIndex, bytes32 solution) returns(bool){}
+
+  function postTask(bytes32 dataRoot, uint timeout, uint reward, uint minDeposit) returns(bytes32, address, uint, uint, uint) {
+  	return (dataRoot, msg.sender, minDeposit, reward, timeout);
+  }
 }
--- test/taskgiver.js
+++ test/taskgiver.js
@@ -1,7 +1,37 @@
 var TaskGiver = artifacts.require("./ExampleTaskGiver.sol");
 var Truebit = artifacts.require("./Truebit.sol");
+var TaskGiver2 = artifacts.require("./TaskGiver.sol");
+
+//Need timeouts or else testrpc will throw invalid opcode errors nondeterministically
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
 
 contract('TaskGiver Integration', function(accounts) {
+
+	it("get instance", function() {
+		return TaskGiver.deployed()
+	});
+
+	setTimeoutPromise(10).then(function(){
+		it("post task", function() {
+			var dataRoot = "0x0000000000000000000000000000000000000000000000000012345678901234";
+			var minDeposit = 1000000000000000000 //1 ETH
+			var reward = 1000000000
+			var timeout = 20; //20 seconds
+			var taskGiver;
+			return TaskGiver2.deployed().then(function(_taskGiver) {
+				taskGiver = _taskGiver
+				return taskGiver.postTask(dataRoot, timeout, reward, minDeposit);
+			}).then(function(task) {
+	      assert.equal(task[0], dataRoot)
+	      assert.equal(task[1], taskGiver.address)
+	      assert.equal(task[2].toNumber(), minDeposit)
+	      assert.equal(task[3].toNumber(), reward, "'reward'")
+	      assert.equal(task[4].toNumber(), timeout)
+			});
+		});
+	});
+
   it("get instance, check truebit address", function() {
     var truebit, taskGiver, dataRoot, minDeposit, value, taskIndex0, globalRoot0, solutions0
     return Truebit.deployed().then(function(_truebit) {
