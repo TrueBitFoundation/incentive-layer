7a7c41691df2972b0da7ac61e9be2768f63e1113
hswick <hswick@smu.edu>
2017-09-29 00:13:37 -0500

    Removed crypto-js dependency and package.json

--- scripts/solverClient.js
+++ scripts/solverClient.js
@@ -1,8 +1,11 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var Solver = artifacts.require("./Solver.sol");
 
 module.exports = function(address) {
 	this.address = address;
+	this.solver = Solver.at(address);
+
 	this.getBlockHash = function() {
 		return web3.eth.getBlock(web3.eth.blockNumber).hash;
 	}
--- test/solver.js
+++ test/solver.js
@@ -1,7 +1,6 @@
 var Solver = artifacts.require("./Solver.sol");
 var solverClient = require("../scripts/solverClient.js");
 var SolverClient = new solverClient();
-var SHA256 = require("crypto-js/sha256");
 
 //Need timeouts or else testrpc will throw invalid opcode errors nondeterministically
 const util = require('util');
@@ -11,4 +10,8 @@ contract('Solver tests', function(accounts) {
 	it("tests solver getting block hash", function() {
 		console.log(SolverClient.getBlockHash());
     })
+
+    it("tests inputting address", function() {
+    	console.log(Solver.deployed().address)
+    });
 });
