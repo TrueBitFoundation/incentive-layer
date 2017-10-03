2aa5d2dae8c55a2201c3bb82c4e0199ca31714b7
hswick <hswick@smu.edu>
2017-10-03 15:49:11 -0500

    Progress with test exchange

--- test/exchange.js
+++ test/exchange.js
@@ -4,7 +4,7 @@ var Verifier = artifacts.require("./Verifier.sol");
 
 contract('TrueBit Exchange', function(accounts) {
   it("should simulate the exchange of ether for computation and verification", function() {
-  	var taskGiver, solver, verifier;
+  	var taskGiver, solver, verifier, sendTask, sendSolution;
   	return TaskGiver.deployed().then(function(_taskGiver) {
   		taskGiver = _taskGiver;
   		return Solver.deployed();
@@ -15,23 +15,46 @@ contract('TrueBit Exchange', function(accounts) {
   		verifier = _verifier;
   		return initializeAccounts([taskGiver, solver, verifier], accounts);
   	}).then(function() {
-
-  		//setup event handlers
-  		var sendTask = taskGiver.SendTask();
-  		sendTask.watch(function(error, result) {
-  			console.log(result);
-  		});
-
-  		var submitSolution = solver.SendSolution();
-  		submitSolution.watch(function(error, result) {
-  			console.log(result);
-  		});
-
   		return new Promise(function(resolve) {
-  			setTimeout(resolve, 2000);
+
+	  		//setup event handlers
+	  		sendTask = taskGiver.SendTask();
+	  		sendTask.watch(function(error, result) {
+	  			if(!error) {
+		  			var from = result.args._from;
+		  			var taskID = result.args.id.toNumber();
+		  			var minDeposit = result.args.minDeposit.toNumber();
+		  			if(minDeposit >= 6000) {
+		  				console.log(from);
+		  				console.log(taskID);
+		  				console.log(minDeposit);
+		  				//solver.sendBid(from, taskID, minDeposit, {from: accounts[6]})
+		  			}
+	  			}else{
+	  				console.error(error);
+	  			}
+	  		});
+
+	  		submitSolution = solver.SendSolution();
+	  		submitSolution.watch(function(error, result) {
+	  			if(!error) {
+	  				var from = result.args._from;
+	  				var taskID = result.args.id.toNumber();
+	  				var solution = result.args.solution;
+	  				var minDeposit = result.args.minDeposit.toNumber();
+					console.log(from);
+					console.log(taskID);
+					console.log(solution);
+					console.log(minDeposit);
+	  			}
+	  		});
+
+	  		resolve();
   		});
   	}).then(function() {
   		return taskGiver.sendTask(6000, {from: accounts[5]});
+  	}).then(function(tx) {
+
   	});
   });
 });
@@ -39,8 +62,8 @@ contract('TrueBit Exchange', function(accounts) {
 function initializeAccounts(accountManagers, accounts) {
 	return new Promise(function(resolve) {
 		for(i = 0; i < accountManagers.length; i++) {
-			for(j = 5; j < 9; j++) {
-				accountManagers[i].submitDeposit({value: 10000, from: accounts[j]});
+			for(j = 5; j < 8; j++) {
+				accountManagers[i].submitDeposit({value: 100000, from: accounts[j]});
 			}
 		}
 		resolve();
