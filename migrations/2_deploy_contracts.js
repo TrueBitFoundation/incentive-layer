ef7c026db1b797abb3623c07b92ff42d058af29e
hswick <hdswick@gmail.com>
2018-02-22 16:17:58 -0600

    Created test jackpot manager to test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var IncentiveLayer = artifacts.require("./IncentiveLayer.sol");
+var IncentiveLayer = artifacts.require("./IncentiveLayer.sol")
+var TestJackpotManager = artifacts.require("./TestJackpotManager.sol")
 
-module.exports = function(deployer, network, thing) {
-  deployer.deploy(IncentiveLayer);
-};
+module.exports = function(deployer) {
+  deployer.deploy(IncentiveLayer)
+  deployer.deploy(TestJackpotManager)
+}
--- test/jackpot_manager.js
+++ test/jackpot_manager.js
@@ -1,16 +1,16 @@
 
-const IncentiveLayer = artifacts.require('IncentiveLayer.sol');
+const TestJackpotManager = artifacts.require('TestJackpotManager.sol');
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 contract('JackpotManager', function(accounts) {
-  let jackpotManager;
+  let jackpotManager, oldBalance, newBalance
 
   before(async () => {
-      jackpotManager = await IncentiveLayer.new()
+      jackpotManager = await TestJackpotManager.new()
   })
   
-  describe('donateToJackpot', () => {
+  describe('interact with Test Jackpot Manager', () => {
     it('should donate to the jackpot', async () => {
       const tx = await jackpotManager.donateToJackpot({from: accounts[1], value: 1000});
       log = tx.logs.find(log => log.event === 'JackpotIncreased')
@@ -18,6 +18,36 @@ contract('JackpotManager', function(accounts) {
 
       const jackpot = await jackpotManager.getJackpotAmount.call();
       assert.equal(jackpot.toNumber(), 1000);
+    })
+
+    it('should distribute jackpot', async () => {
+      await jackpotManager.distributeJackpot([accounts[0], accounts[1]], [accounts[2], accounts[3]])
+
+      const jackpotID = (await jackpotManager.getCurrentJackpotID.call()).toNumber()
+
+      assert.equal(jackpotID, 1)
+    })
+
+    it('should be able to receive payment', async () => {
+      oldBalance = await web3.eth.getBalance(accounts[0])
+      await jackpotManager.receiveJackpotPayment(0, 0, 0, {from: accounts[0]})
+      newBalance = await web3.eth.getBalance(accounts[0])
+      assert.notEqual(oldBalance, newBalance)
+
+      oldBalance = await web3.eth.getBalance(accounts[1])
+      await jackpotManager.receiveJackpotPayment(0, 0, 1, {from: accounts[1]})
+      newBalance = await web3.eth.getBalance(accounts[1])
+      assert.notEqual(oldBalance, newBalance)
+
+      oldBalance = await web3.eth.getBalance(accounts[2])
+      await jackpotManager.receiveJackpotPayment(0, 1, 0, {from: accounts[2]})
+      newBalance = await web3.eth.getBalance(accounts[2])
+      assert.notEqual(oldBalance, newBalance)
+
+      oldBalance = await web3.eth.getBalance(accounts[3])
+      await jackpotManager.receiveJackpotPayment(0, 1, 1, {from: accounts[3]})
+      newBalance = await web3.eth.getBalance(accounts[3])
+      assert.notEqual(oldBalance, newBalance)
     })
   })
 })
