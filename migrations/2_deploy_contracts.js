126418bc3fae61c48340e952d07d8e047486a552
hswick <hdswick@gmail.com>
2018-04-03 15:00:39 -0500

    Stripped down task exchange

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
-var IncentiveLayer = artifacts.require("./IncentiveLayer.sol")
-var TestJackpotManager = artifacts.require("./TestJackpotManager.sol")
+const IncentiveLayer = artifacts.require("./IncentiveLayer.sol")
+const TestJackpotManager = artifacts.require("./TestJackpotManager.sol")
+const TaskExchange = artifacts.require("./TaskExchange.sol")
+
 
 module.exports = function(deployer) {
   deployer.deploy(IncentiveLayer)
   deployer.deploy(TestJackpotManager)
+  deployer.deploy(TaskExchange)
 }
