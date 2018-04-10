19d8917c77d3369690fafe2f252e5dfee59d0f1e
hswick <hdswick@gmail.com>
2018-04-10 14:28:19 -0500

    Create export directory with artifacts on migration

--- migrations/3_write_addresses.js
+++ migrations/3_write_addresses.js
@@ -1,8 +1,24 @@
 const fs = require('fs')
 const IncentiveLayer = artifacts.require("./IncentiveLayer.sol")
 
-module.exports = (deployer) => {
-  fs.writeFile(__dirname + '/../client/addresses.json', JSON.stringify({
-    incentiveLayer: IncentiveLayer.address
-  }), (e) => {if(e) console.error(e) })
+module.exports = (deployer, network) => {
+  let exportedContracts = {}
+
+  let contracts = [IncentiveLayer]
+
+  contracts.forEach((contract) => {
+
+    exportedContracts[contract.contractName] = {
+      abi: contract.abi,
+      address: contract.address
+    }
+  })
+
+  if (!fs.existsSync(__dirname + "/../export/")){
+    fs.mkdirSync(__dirname + "/../export/")
+  }
+
+  let path = __dirname + "/../export/" + network+ ".json"
+
+  fs.writeFile(path, JSON.stringify(exportedContracts), (e) => {if(e) console.error(e) })
 }
