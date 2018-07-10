6e1ab73dea54022f1ebe5ea15f664787ad197e3c
Surya Bakshi <sbaks0820@gmail.com>
2018-07-10 15:24:06 -0400

    exchange rate oracle is queried to determine min deposit in incentive layer with tests updates

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,14 +3,25 @@ const TestJackpotManager = artifacts.require("./TestJackpotManager.sol")
 const TaskExchange = artifacts.require("./TaskExchange.sol")
 const DisputeResolutionLayerDummy = artifacts.require("./DisputeResolutionLayerDummy.sol")
 const TRU = artifacts.require('./TRU.sol')
+const ExchangeRateOracle = artifacts.require('./ExchangeRateOracle.sol')
 
+//module.exports = function(deployer, network, accounts) {
+//    deployer.deploy(TestJackpotManager)
+//    deployer.deploy(TaskExchange)
+//    deployer.deploy(DisputeResolutionLayerDummy)
+//    deployer.deploy(ExchangeRateOracle)    
+//    deployer.deploy(TRU).then( function () {
+//        deployer.deploy(IncentiveLayer, TRU.address, ExchangeRateOracle.address, {from: accounts[0]});
+//    });
+//}
 
-module.exports = function(deployer, network, accounts) {
+module.exports = async (deployer, network, accounts) => {
     deployer.deploy(TestJackpotManager)
     deployer.deploy(TaskExchange)
     deployer.deploy(DisputeResolutionLayerDummy)
-    deployer.deploy(TRU).then( function () {
-        deployer.deploy(IncentiveLayer, TRU.address, {from: accounts[0]});
-    });
-        
+    deployer.deploy(ExchangeRateOracle)    
+    await deployer.deploy(TRU)
+    await deployer.deploy(ExchangeRateOracle)
+    await deployer.deploy(IncentiveLayer, TRU.address, ExchangeRateOracle.address, {from: accounts[0]});
 }
+    
